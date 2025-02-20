6fab890e69e8dca707962da21ee2b4b22f7d916b
JitendraPatidar2511 <150067636+JitendraPatidar2511@users.noreply.github.com>
2025-02-20 16:37:09 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,47 @@
+//  Variables
+
+// using var (function-scoped and can be re-assigned)
+var Firstname = "Jitendra";   
+console.log(Firstname); /* output = jitendra */
+Firstname = "aadi";  /*re-assign*/
+console.log(Firstname);  /*output = aadi */
+
+
+//using let (block-scoped and can be re-assigned)
+let Lastname = "Patidar";
+console.log(Lastname);  /* output=Patidar */
+Lastname = "patel";  /* re-assign*/
+console.log(Lastname);  /* output=patel*/
+
+//using const (block-scoped and can't be re-assigned)
+const City = 30;
+console.log(City);
+
+
+/*1.  Create a js file which contains variable declaration for a form which asks following basic details :
+(What is your Name, Email, age, address, phone number, gender,
+ Are you married) */
+
+let Name = "jitendra";
+console.log(Name);
+
+let Email = "jit11@gmail.com";
+console.log(Email);
+
+const age = 50;
+console.log(age);
+
+let address = "nanda nagar";
+console.log(address);
+
+let phonenumber = "511564646";
+console.log(phonenumber);
+
+let gender = "xyz"
+console.log(gender);
+
+
+
+// 2. Add comments on the top of the file to note this question as well.
+/*Add single line comment to use //
+and for multiple line use /* */
