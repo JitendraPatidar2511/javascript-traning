b7d64dbad5805d1f12ad17792dc3287671d46e4f
JitendraPatidar2511 <150067636+JitendraPatidar2511@users.noreply.github.com>
2025-02-21 14:06:19 +0530

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,51 @@
+// 1. Declare all the variables in above form without initialising and print their value.
+
+/*let Name;
+console.log(Name);
+
+let Email;
+console.log(Email);
+
+let age;
+console.log(age);
+
+let address;
+console.log(address);
+
+let phonenumber;
+console.log(phonenumber);
+
+let gender;
+ console.log(gender);   
+//  (All output can show undefined)
+ 
+//2. After printing their value assign them as null and print it again.
+ let Name = null;
+ console.log(Name);
+
+ let Email = null;
+ console.log(Email);
+
+ let age = null;
+ console.log(age);
+
+ let address = null;
+ console.log(address);
+
+ let phonenumber = null;
+ console.log(phonenumber);
+
+ let gender = null;
+ console.log(gender);   
+// (All output can show null  )
+
+// 3.Declare an object called employeeDetails and add all the above variables to this object now as keys with value as null and print this object. 
+
+let employeeDetails = {
+    Name:null,
+    age:null,
+    Email:null,
+    address:null,
+}
+console.log(employeeDetails);  */
+// (output = Name: null, age: null, Email: null, address: null )
